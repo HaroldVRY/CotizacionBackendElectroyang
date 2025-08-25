fc5ee860d1bd65bdb572cfb941001945aec54843
HaroldVRY <haroldreynayangali@gmail.com>
2025-08-25 16:56:45 -0500

    Contrasena

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432'),
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || '12345',
   database: process.env.DB_NAME || 'electroyang_cotizaciones'
 };
 
